8c1d9a1a866dec33f8a6f233560bc8f2731deade
VishwajeethV <74006085+VishwajeethV@users.noreply.github.com>
2020-11-19 17:59:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload() {
+
+
+}
+
+function setup() {
+
+    canvas = createCanvas(300,250);
+    canvas.center();
+    
+}
+
+function draw() {
+
+
+}
+
+function take_snapshot() {
+
+ save("clown_nose.jpg");
+}
